021822b54882e8e945bf6b5d9c1829ee9ae8ffd9
Petrutiu Mihai <mihaipetrutiu@yahoo.com>
2017-01-17 09:46:51 +0200

    Add examples

---
+++ FirstExample.py
@@ -0,0 +1,14 @@
+import tensorflow as tf
+
+x1 = tf.constant(5)
+x2 = tf.constant(6)
+
+#result = x1 * x2
+
+result = tf.mul(x1, x2)
+
+print(result)
+
+with tf.Session() as session:
+    output = session.run(result)
+    print(output)
